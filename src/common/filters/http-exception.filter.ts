ea739b8e9f57fe0303f297b2e321416b9dd6ba55
Daniel Araújo <daniel.ti.info@gmail.com>
2025-10-24 10:07:14 -0300

    feat: filtro global para padronização de erros no backend

--- src/common/filters/http-exception.filter.ts
+++ src/common/filters/http-exception.filter.ts
@@ -1,6 +1,27 @@
-import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from '@nestjs/common';
 
 @Catch()
 export class HttpExceptionFilter<T> implements ExceptionFilter {
-  catch(exception: T, host: ArgumentsHost) {}
+  catch(exception: T, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+
+    const status = 
+     exception instanceof HttpException
+     ? exception.getStatus()
+     : HttpStatus.INTERNAL_SERVER_ERROR;
+
+     const message = 
+       exception instanceof HttpException
+       ? (exception.getResponse() as any).message ||
+       exception.message ||
+       'Erro inesperado.'
+       : 'Erro interno no servidor.';
+
+       response.status(status).json({
+        success: false,
+        statusCode: status,
+        message,
+       });
+  }
 }
