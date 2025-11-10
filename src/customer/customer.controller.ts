03f9fca54843f4d9f7bc30ba97fbf5620bef7e4d
Daniel Araújo <daniel.ti.info@gmail.com>
2025-11-10 17:34:09 -0300

    refactor: customerService

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -4,23 +4,23 @@ import { CreateCustomerDTO } from './dto/create-customer.dto';
 
 @Controller('customers')
 export class CustomerController {
-    constructor(private readonly customersService: CustomerService) { }
+    constructor(private readonly customerService: CustomerService) { }
 
     @Post('register')
     async register(@Body() dto: CreateCustomerDTO) {
-        const newCustomer = await this.customersService.create(dto);
+        const newCustomer = await this.customerService.create(dto);
         const { password_hash, verification_code, ...safeCustomer } = newCustomer;
         return { message: 'Cliente cadastrado com sucesso!', customer: safeCustomer };
     };
 
     @Get()
     async findAll() {
-        return this.customersService.findAll();
+        return this.customerService.findAll();
     }
 
     @Get(':id')
     async findOne(@Param('id') id: string) {
-        const customer = await this.customersService.findOne(id);
+        const customer = await this.customerService.findOne(id);
         if (!customer) {
             throw new NotFoundException('Cliente não encontrado');
         }
@@ -31,21 +31,14 @@ export class CustomerController {
     @Patch(':id')
     @UsePipes(new ValidationPipe({ skipMissingProperties: true }))
     async update(@Param('id') id: string, @Body() data: Partial<CreateCustomerDTO>) {
-        const updated = await this.customersService.update(id, data);
+        const updated = await this.customerService.update(id, data);
         const { password_hash, verification_code, ...safeCustomer } = updated;
         return { message: 'Cliente atualizado com sucesso!', customer: safeCustomer };
     }
 
-    /* Restaurar cliente antigo caso necessário
-    @Patch(':id/restore')
-    async restore(@Param('id', new ParseUUIDPipe()) id: string) {
-        await this.customersService.restore(id);
-        return { message: 'Cliente restaurado com sucesso!'};
-    } */
-
     @Delete(':id')
     async remove(@Param('id', new ParseUUIDPipe()) id: string) {
-        await this.customersService.remove(id);
+        await this.customerService.remove(id);
         return { message: 'Cliente excluído com sucesso!' };
     }
 }
