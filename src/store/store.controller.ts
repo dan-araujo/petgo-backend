d6a0c72ad27f5a3d92e1606c7706d03a20ce0b01
Daniel Araújo <77083737+dan-araujo@users.noreply.github.com>
2025-12-09 17:27:43 -0300

    refactor(store): adds the standard response interface for the AP
    - Add support for asynchronous operations in controller methods
    - Maintains compatibility with the standard ApiResponse
    -  Add ParseUUIDPipe to id validation

--- src/store/store.controller.ts
+++ src/store/store.controller.ts
@@ -1,21 +1,22 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe, NotFoundException } from '@nestjs/common';
 import { StoreService } from './store.service';
 import { CreateStoreDTO } from './dto/create-store.dto';
 import { UpdateStoreDTO } from './dto/update-store.dto';
 import { Store } from './entities/store.entity';
+import { ApiResponse } from '../common/interfaces/api-response.interface';
 
 @Controller('stores')
 export class StoreController {
   constructor(private readonly storeService: StoreService) { }
 
   @Post('register')
-  async create(@Body() dto: CreateStoreDTO): Promise<{ message: string; store: Partial<Store> }> {
+  async create(@Body() dto: CreateStoreDTO): Promise<ApiResponse<Partial<Store>>> {
     const newStore = await this.storeService.create(dto);
     const { password_hash, verification_code, ...safeStore } = newStore;
 
     return {
       message: 'Loja cadastrada com sucesso!',
-      store: safeStore,
+      data: safeStore,
     };
   }
 
@@ -25,18 +26,30 @@ export class StoreController {
   }
 
   @Get(':id')
-  async findOne(@Param('id') id: string): Promise<Store> {
-    return this.storeService.findOne(id);
+  async findOne(@Param('id') id: string) {
+    const store = await this.storeService.findOne(id);
+    if (!store) {
+          throw new NotFoundException('Loja não encontrada');
+        }
+        const { password_hash, verification_code, ...safeStore } = store;
+        return { data: safeStore }
   }
 
   @Patch(':id')
-  async update(@Param('id') id: string, @Body() dto: UpdateStoreDTO): Promise<Store> {
-    return this.storeService.update(id, dto);
+  async update(@Param('id', ParseUUIDPipe) id: string, @Body() dto: UpdateStoreDTO):
+    Promise<ApiResponse<Partial<Store>>> {
+    const updatedStore = await this.storeService.update(id, dto);
+    const { password_hash, verification_code, ...safeStore } = updatedStore;
+
+    return {
+      message: 'Loja atualizada com sucesso!',
+      data: safeStore,
+    };
   }
 
   @Delete(':id')
-  async remove(@Param('id') id: string): Promise<{ message: string }> {
+  async remove(@Param('id') id: string): Promise<ApiResponse<Partial<void>>> {
     await this.storeService.remove(id);
-    return { message: 'Loja removida com sucesso.' };
+    return { message: 'Loja excluída com sucesso.' };
   }
 }
