00d02d9061a5ee09a1bba523a8f396e5d869f094
Daniel Araújo <77083737+dan-araujo@users.noreply.github.com>
2025-12-09 17:27:05 -0300

    refactor(delivery): adds the standard response interface for the API
    - Add support for asynchronous operations in controller methods
    - Maintains compatibility with the standard ApiResponse
    -  Add ParseUUIDPipe to id validation

--- src/delivery/delivery.controller.ts
+++ src/delivery/delivery.controller.ts
@@ -1,21 +1,22 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe, NotFoundException } from '@nestjs/common';
 import { DeliveryService } from './delivery.service';
 import { CreateDeliveryDTO } from './dto/create-delivery.dto';
 import { UpdateDeliveryDTO } from './dto/update-delivery.dto';
 import { Delivery } from './entities/delivery.entity';
+import { ApiResponse } from '../common/interfaces/api-response.interface';
 
 @Controller('delivery')
 export class DeliveryController {
   constructor(private readonly deliveryService: DeliveryService) { }
 
   @Post('register')
-  async create(@Body() dto: CreateDeliveryDTO): Promise<{ message: string; delivery: Partial<Delivery> }> {
+  async create(@Body() dto: CreateDeliveryDTO): Promise<ApiResponse<Partial<Delivery>>> {
     const newDelivery = await this.deliveryService.create(dto);
     const { password_hash, verification_code, ...safeDelivery } = newDelivery;
 
     return {
-      message: 'Entregador cadastro com sucesso!',
-      delivery: safeDelivery,
+      message: 'Entregador cadastrado com sucesso!',
+      data: safeDelivery,
     };
   }
 
@@ -25,17 +26,28 @@ export class DeliveryController {
   }
 
   @Get(':id')
-  async findOne(@Param('id') id: string): Promise<Delivery> {
-    return this.deliveryService.findOne(id);
+  async findOne(@Param('id') id: string) {
+    const delivery = await this.deliveryService.findOne(id);
+    if (!delivery) {
+      throw new NotFoundException('Entregador não encontrado');
+    }
+    const { password_hash, verification_code, ...safeDelivery } = delivery;
+    return { data: safeDelivery }
   }
 
   @Patch(':id')
-  async update(@Param('id') id: string, @Body() dto: UpdateDeliveryDTO): Promise<Delivery> {
-    return this.deliveryService.update(id, dto);
+  async update(@Param('id', ParseUUIDPipe) id: string, @Body() dto: UpdateDeliveryDTO):
+    Promise<ApiResponse<Partial<Delivery>>> {
+    const updatedDelivery = await this.deliveryService.update(id, dto);
+    const { password_hash, verification_code, ...safeDelivery } = updatedDelivery;
+    return {
+      message: 'Entregador atualizado com sucesso!',
+      data: safeDelivery,
+    };
   }
 
   @Delete(':id')
-  async remove(@Param('id') id: string): Promise<{ message: string }> {
+  async remove(@Param('id') id: string): Promise<ApiResponse<void>> {
     await this.deliveryService.remove(id);
     return { message: 'Entregador removido com sucesso.' }
   }
