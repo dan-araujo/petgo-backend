cb38534008702d5fad05e9e6ab6e7d53c16481e5
Daniel Araújo <77083737+dan-araujo@users.noreply.github.com>
2025-12-09 17:28:10 -0300

    refactor(veterinary): adds the standard response interface for the AP
    - Add support for asynchronous operations in controller methods
    - Maintains compatibility with the standard ApiResponse
    -  Add ParseUUIDPipe to id validation

--- src/veterinary/veterinary.controller.ts
+++ src/veterinary/veterinary.controller.ts
@@ -2,42 +2,55 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, NotFoundException, P
 import { VeterinaryService } from './veterinary.service';
 import { CreateVeterinaryDTO } from './dto/create-veterinary.dto';
 import { UpdateVeterinaryDTO } from './dto/update-veterinary.dto';
+import { ApiResponse } from '../common/interfaces/api-response.interface';
+import { Veterinary } from './entities/veterinary.entity';
 
 @Controller('veterinaries')
 export class VeterinaryController {
-  constructor(private readonly veterinaryService: VeterinaryService) {}
+  constructor(private readonly veterinaryService: VeterinaryService) { }
 
   @Post('register')
-  create(@Body() createVetDto: CreateVeterinaryDTO) {
-    return this.veterinaryService.create(createVetDto);
+  async create(@Body() dto: CreateVeterinaryDTO): Promise<ApiResponse<Partial<Veterinary>>> {
+    const newVeterinary = await this.veterinaryService.create(dto);
+    const { password_hash, verification_code, ...safeVeterinary } = newVeterinary;
+    return {
+      message: 'Veterinário cadastrado com sucesso!',
+      data: safeVeterinary,
+    };
   }
 
   @Get()
-  findAll() {
+  async findAll(): Promise<Veterinary[]> {
     return this.veterinaryService.findAll();
   }
 
   @Get(':id')
-async findOne(@Param('id') id: string) {
-  const veterinary = await this.veterinaryService.findOne(id);
-  if (!veterinary) {
-    throw new NotFoundException('Veterinário não encontrado');
+  async findOne(@Param('id', ParseUUIDPipe) id: string) {
+    const veterinary = await this.veterinaryService.findOne(id);
+    if (!veterinary) {
+      throw new NotFoundException('Veterinário não encontrado.');
+    }
+    const { password_hash, verification_code, ...safeVeterinary } = veterinary;
+    return { data: safeVeterinary }
   }
-  const { password_hash, verification_code, ...safeVeterinary } = veterinary;
-  return safeVeterinary;
-}
 
   @Patch(':id')
   @UsePipes(new ValidationPipe({ skipMissingProperties: true }))
-  async update(@Param('id') id: string, @Body() data: Partial<UpdateVeterinaryDTO>) {
-    const updated = await this.veterinaryService.update(id, data);
-    const { password_hash, verification_code, ...safeVeterinary } = updated;
-    return { message: 'Veterinário atualizado com sucesso!', veterinary: safeVeterinary };
+  async update(@Param('id') id: string, @Body() dto: Partial<UpdateVeterinaryDTO>):
+    Promise<ApiResponse<Partial<Veterinary>>> {
+    const updatedVeterinary = await this.veterinaryService.update(id, dto);
+    const { password_hash, verification_code, ...safeVeterinary } = updatedVeterinary;
+    return {
+      message: 'Veterinário atualizado com sucesso!',
+      data: safeVeterinary
+    };
   }
 
   @Delete(':id')
-  async remove(@Param('id', new ParseUUIDPipe()) id: string) {
+  async remove(@Param('id', new ParseUUIDPipe) id: string): Promise<ApiResponse<void>> {
     await this.veterinaryService.remove(id);
-    return { message: 'Veterinário excluído com sucesso!' }
+    return {
+      message: 'Veterinário excluído com sucesso!',
+    };
   }
 }
