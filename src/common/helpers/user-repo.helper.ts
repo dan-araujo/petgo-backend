690f446dda9e56fa0a435609e9ba5e87e7078875
Daniel Araújo <77083737+dan-araujo@users.noreply.github.com>
2025-12-15 18:38:31 -0300

    feat(auth): update helper to handle all user types

--- src/common/helpers/user-repo.helper.ts
+++ src/common/helpers/user-repo.helper.ts
@@ -1,20 +1,32 @@
 import { Repository } from "typeorm";
 import { Store } from "../../store/entities/store.entity";
+import { Customer } from "../../customer/entities/customer.entity";
+import { Delivery } from "../../delivery/entities/delivery.entity";
+import { Veterinary } from "../../veterinary/entities/veterinary.entity";
 
 export type UserType = 'store' | 'customer' | 'delivery' | 'veterinary';
 
 export class UserRepoHelper {
-    static getRepo(
-     type: UserType,
-     repos: {
-        storeRepo?: Repository<Store>;
-     }
-    ): Repository<any> {
-      switch(type) {
-        case 'store':
-            return repos.storeRepo!;
-        default:
-            throw new Error(`Tipo de usuário inválido ou não implementado: ${type}`);
-      }
+  static getRepo(
+    type: UserType,
+    repos: {
+      storeRepo?: Repository<Store>;
+      customerRepo?: Repository<Customer>;
+      deliveryRepo?: Repository<Delivery>
+      veterinaryRepo?: Repository<Veterinary>;
     }
+  ): Repository<any> {
+    switch (type) {
+      case 'store':
+        return repos.storeRepo!;
+      case 'customer':
+        return repos.customerRepo!;
+      case 'delivery':
+        return repos.deliveryRepo!;
+      case 'veterinary':
+        return repos.veterinaryRepo!;
+      default:
+        throw new Error(`Tipo de usuário inválido ou não implementado: ${type}`);
+    }
+  }
 }
