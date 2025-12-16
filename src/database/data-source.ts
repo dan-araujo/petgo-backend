948fee338e1be6455d4484030757ce6da512e6a0
Daniel Araújo <77083737+dan-araujo@users.noreply.github.com>
2025-12-15 23:31:49 -0300

    chore(database): add data source to TypeORM CLI

---
+++ src/database/data-source.ts
@@ -0,0 +1,14 @@
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  entities: ['src/**/*.entity.ts'],
+  migrations: ['src/migrations/*.ts'],
+  subscribers: ['src/**/*.subscriber.ts'],
+  synchronize: false,
+  logging: process.env.NODE_ENV === 'development',
+});
