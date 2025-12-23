c53f4c6bff1f6b170660fd35668847b202d956c9
Daniel Araújo <77083737+dan-araujo@users.noreply.github.com>
2025-12-23 20:22:19 -0300

    refactor(config): add timeout variables

--- src/config/mailer.config.ts
+++ src/config/mailer.config.ts
@@ -3,15 +3,22 @@ import { ConfigService } from "@nestjs/config";
 
 export const mailerConfig = (configService: ConfigService): MailerOptions => {
     const mailSecure = configService.get<string>('MAIL_SECURE', 'true') === 'true';
+    const mailPort = parseInt(configService.get<string>('MAIL_PORT', '465'));
 
     return {
         transport: {
             host: configService.get<string>('MAIL_HOST'),
-            port: parseInt(configService.get<string>('MAIL_PORT', '465')), 
+            port: mailPort,
             secure: mailSecure, 
+            connectionTimeout: 10000, 
+            greetingTimeout: 10000,   
+            socketTimeout: 10000,    
             auth: {
                 user: configService.get<string>('MAIL_USER'),
                 pass: configService.get<string>('MAIL_PASSWORD'),
+            },
+            tls: {
+                rejectUnauthorized: false,
             },
         },
         defaults: {
