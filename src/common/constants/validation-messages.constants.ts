81fdbb6759013a2c6bb6b7a59e61053df2ece061
Daniel Araújo <77083737+dan-araujo@users.noreply.github.com>
2026-01-06 15:28:37 -0300

    refactor(constants): rename file to better identification of its responsibility

---
+++ src/common/constants/validation-messages.constants.ts
@@ -0,0 +1,29 @@
+export const ValidationMessages = {
+
+    MINIMUM_SIZE: (min: number) => `Este campo deve ter pelo menos ${min} caracteres.`,
+    MAXIMUM_SIZE: (max: number) => `Este campo deve ter no máximo ${max} caracteres.`,
+    SHORT_PASSWORD: (min: number) => `A senha deve ter pelo menos ${min} caracteres.`,
+    VERIFICATION_CODE: (min: number) => `Código deve ter exatamente ${min} digitos.`,
+    SHORT_NAME: 'O nome deve ter pelo menos 3 caracteres.',
+
+    REQUIRED_FIELD: 'Este campo é obrigatório.',
+    REQUIRED_NAME: 'O nome é obrigatório.',
+    REQUIRED_PHONE: 'Telefone é obrigatório',
+    REQUIRED_PASSWORD: 'A senha é obrigatoria.',
+    REQUIRED_CNPJ: 'Preencha seu CNPJ',
+    REQUIRED_CPF: 'Preencha seu CPF',
+    REQUIRED_EMAIL: 'O e-mail é obrigatório.',
+    REQUIRED_USER_TYPE: 'Tipo de usuário é obrigatório',
+ 
+    INVALID_EMAIL: 'O e-mail informado é inválido.',
+    INVALID_PHONE: 'O telefone informado é inválido.',
+    INVALID_CATEGORY: 'Selecione uma das opções',
+    INVALID_CPF: 'O CPF informado é inválido.',
+    INVALID_CNPJ: 'O CNPJ informado é inválido',
+    INVALID_USER_TYPE: 'Tipo de usuário inválido',
+
+    EMAIL_ALREADY_EXISTS: 'Já existe um cadastro com este e-mail',
+    PHONE_ALREADY_EXISTS: 'Já existe um cadastro com este telefone.',
+    CNPJ_ALREADY_EXISTS: 'Já existe uma loja com este CNPJ',
+    CPF_ALREADY_EXISTS: 'CPF já cadastrado'
+};
