6aacb616e67061a987c68f45dbe1701b6c549374
Daniel Araújo <77083737+dan-araujo@users.noreply.github.com>
2026-01-06 15:27:00 -0300

    refactor(common): centralizes the modules for a shared infraestructure

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -5,12 +5,14 @@ import { Delivery } from "../modules/delivery/entities/delivery.entity";
 import { Store } from "../modules/store/entities/store.entity";
 import { Veterinary } from "../modules/veterinary/entities/veterinary.entity";
 import { UserReposityResolver } from "./services/user-repository.resolver";
+import { MailgunEmailService } from "./services/mailgun-email.service";
+import { EmailService } from "./services/email.service";
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Customer, Delivery, Store, Veterinary]),
   ],
-  providers: [UserReposityResolver],
-  exports: [UserReposityResolver],
+  providers: [UserReposityResolver, MailgunEmailService, EmailService],
+  exports: [UserReposityResolver, MailgunEmailService, EmailService],
 })
 export class CommonModule {}
