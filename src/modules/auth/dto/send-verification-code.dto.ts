493fa990279e5abf669677c0d8f881981bd18227
Daniel Araújo <77083737+dan-araujo@users.noreply.github.com>
2026-01-05 21:24:36 -0300

    move/rename files

--- src/modules/auth/dto/send-verification-code.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
-import { ValidationMessages } from '../../../common/constants/validation-messages';
-
-export class ResendVerificationCodeDTO {
-  @IsNotEmpty({ message: ValidationMessages.REQUIRED_EMAIL })
-  @IsEmail({}, { message: ValidationMessages.INVALID_EMAIL })
-  email: string;
-}
