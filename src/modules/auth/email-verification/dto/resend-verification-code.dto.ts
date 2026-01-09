a90f05a775a5becb0f0233a2558e8f8f99f17cd1
Daniel Araújo <77083737+dan-araujo@users.noreply.github.com>
2026-01-09 03:45:22 -0300

    fix(email-verification): fix dto annotation for user type

--- src/modules/auth/email-verification/dto/resend-verification-code.dto.ts
+++ src/modules/auth/email-verification/dto/resend-verification-code.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { IsEmail, IsEnum, IsNotEmpty } from 'class-validator';
 import { ValidationMessages } from '../../../../common/constants/validation-messages.constants';
 import { UserType } from '../../../../common/enums/user-type.enum';
 
@@ -8,7 +8,6 @@ export class ResendVerificationCodeDTO {
   @IsEmail({}, { message: ValidationMessages.INVALID_EMAIL })
   email: string;
 
-  @IsNotEmpty({ message: ValidationMessages.REQUIRED_USER_TYPE })
-  @IsEmail({}, { message: ValidationMessages.INVALID_USER_TYPE })
+  @IsEnum(UserType)
   userType: UserType;
 }
--- src/modules/auth/email-verification/dto/verify-email.dto.ts
+++ src/modules/auth/email-verification/dto/verify-email.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty, Length } from 'class-validator';
+import { IsEmail, IsEnum, IsNotEmpty, Length } from 'class-validator';
 import { ValidationMessages } from '../../../../common/constants/validation-messages.constants';
 import { UserType } from '../../../../common/enums/user-type.enum';
 
@@ -13,7 +13,6 @@ export class VerifyEmailDTO {
     @Length(6, 6, { message: ValidationMessages.VERIFICATION_CODE(6) })
     code: string;
 
-    @IsNotEmpty({ message: ValidationMessages.REQUIRED_USER_TYPE })
-    @IsEmail({}, { message: ValidationMessages.INVALID_USER_TYPE })
+    @IsEnum(UserType)
     userType: UserType;
 }
