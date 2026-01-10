60a4e188943de7db283505f916f95b3c3b53cdb5
Daniel Araújo <77083737+dan-araujo@users.noreply.github.com>
2026-01-10 01:50:20 -0300

    fix: remove AuthResponse and standardize auth.controller with ApiResponse

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,17 +1,18 @@
 import { Body, Controller, Param, Post } from '@nestjs/common';
-import { AuthResponse, AuthService } from './auth.service';
+import { AuthService } from './auth.service';
 import { LoginDTO } from './login/dto/login.dto';
 import { UserType } from '../../common/enums/user-type.enum';
-
+import { ApiResponse, LoginSuccessData, VerificationData } from '../../common/interfaces/api-response.interface';
 
 @Controller('auth')
 export class AuthController {
-  constructor(
-    private readonly authService: AuthService,
-  ) { }
+  constructor(private readonly authService: AuthService) {}
 
   @Post('login/:type')
-  async login(@Param('type') type: UserType, @Body() dto: LoginDTO): Promise<AuthResponse> {
+  async login(
+    @Param('type') type: UserType, 
+    @Body() dto: LoginDTO
+  ): Promise<ApiResponse<LoginSuccessData | VerificationData>> {
     return this.authService.login(type, dto);
   }
 }
