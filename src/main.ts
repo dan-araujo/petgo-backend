e6ce2106a3432d90adc3d9b6c47be4f9be3472f4
Daniel Araújo <77083737+dan-araujo@users.noreply.github.com>
2026-01-30 21:29:13 -0300

    feat(main): configure the document builder for swagger api documentation

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,31 @@ import { ValidationPipe } from '@nestjs/common';
 import 'reflect-metadata';
 import { ResponseInterceptor } from './common/interceptors/interceptor';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true, 
-      forbidNonWhitelisted: true, 
-      transform: true, 
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
     }),
   );
 
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('PetGo! API')
+    .setDescription('API para gestão de entregas de produtos para pets e serviços veterinários')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
