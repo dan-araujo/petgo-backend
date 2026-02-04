fd32582b1059a10fd08676a6554ad4a57e723e26
Daniel Araújo <77083737+dan-araujo@users.noreply.github.com>
2026-02-04 20:39:53 -0300

    feat(catalog): implement complete catalog system including products and services

--- src/modules/catalog/catalog.module.ts
+++ src/modules/catalog/catalog.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
-import { CatalogService } from './catalog.service';
+import { CatalogService } from './services/catalog.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Category } from './entities/category.entity';
+import { Product } from './entities/product.entity';
+import { CategoryController } from './controllers/category.controller';
+import { ProductController } from './controllers/product.controller';
+import { ProductService } from './services/product.service';
+import { PetService } from './entities/service.entity';
+import { PetServicesController } from './controllers/pet-services.controller';
+import { PetServicesService } from './services/pet-services.service';
 
 @Module({
-  providers: [CatalogService]
+  imports: [TypeOrmModule.forFeature([Category, Product, PetService])],
+  controllers: [CategoryController, ProductController, PetServicesController],
+  providers: [CatalogService, ProductService, PetServicesService],
+  exports: [CatalogService, ProductService, PetServicesService]
 })
-export class CatalogModule {}
+export class CatalogModule { }
